b65bca3072d6eaf61782a0c6d558cdfaed10627b
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-09 12:44:44 -0300

    feat(CRUD-com-JS-assincrono): add class 01 - lidando com promises

--- CRUD-com-JS-assincrono/admin/service/cliente.service.js
+++ CRUD-com-JS-assincrono/admin/service/cliente.service.js
@@ -19,15 +19,29 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector('[data-tabela]');
 
 
-const http = new XMLHttpRequest();
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    
+    http.open('GET', 'http://localhost:3000/profile');
 
-http.open('GET', 'http://localhost:3000/profile');
+    http.onload = () => {
+      if(http.status >= 400) {
+        reject(JSON.parse(http.response))
+      } else {
+        resolve(JSON.parse(http.response))
+      }
+    }
 
-http.send();
+    http.send();  
+  })
 
-http.onload = () => {
-  const data = JSON.parse(http.response);
-  data.forEach(elemento => {
-    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-  });
-}
+  return promise;
+};
+
+listaClientes()
+  .then(data => {
+    data.forEach(elemento => {
+      tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+    });
+  })
