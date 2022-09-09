8ca33321bb23163342e01b1ce2b983afb3838aba
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-09 12:00:10 -0300

    feat(CRUD-com-JS-assincrono): class 01 - criando template

--- CRUD-com-JS-assincrono/admin/service/cliente.service.js
+++ CRUD-com-JS-assincrono/admin/service/cliente.service.js
@@ -1,3 +1,24 @@
+const criaNovaLinha = (nome, email) => {
+  const linhaNovoCliente = document.createElement('tr');
+  const conteudo = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+      <ul class="tabela__botoes-controle">
+        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+      </ul>
+    </td> 
+  `;
+
+  linhaNovoCliente.innerHTML = conteudo;
+
+  return linhaNovoCliente;
+};
+
+const tabela = document.querySelector('[data-tabela]');
+
+
 const http = new XMLHttpRequest();
 
 http.open('GET', 'http://localhost:3000/profile');
@@ -5,6 +26,8 @@ http.open('GET', 'http://localhost:3000/profile');
 http.send();
 
 http.onload = () => {
-  const data = http.response
-  console.log(data)
+  const data = JSON.parse(http.response);
+  data.forEach(elemento => {
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+  });
 }
