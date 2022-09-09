291b8d835c942af8b33be44d15786300de1cf73d
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-09 14:04:15 -0300

    feat(CRUD-com-JS-assincrono): class 02 - reescrevendo com Fetch API

--- CRUD-com-JS-assincrono/admin/service/cliente.service.js
+++ CRUD-com-JS-assincrono/admin/service/cliente.service.js
@@ -20,23 +20,7 @@ const tabela = document.querySelector('[data-tabela]');
 
 
 const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    
-    http.open('GET', 'http://localhost:3000/profile');
-
-    http.onload = () => {
-      if(http.status >= 400) {
-        reject(JSON.parse(http.response))
-      } else {
-        resolve(JSON.parse(http.response))
-      }
-    }
-
-    http.send();  
-  })
-
-  return promise;
+  return fetch(`http://localhost:3000/profile`).then( resposta => resposta.json());
 };
 
 listaClientes()
