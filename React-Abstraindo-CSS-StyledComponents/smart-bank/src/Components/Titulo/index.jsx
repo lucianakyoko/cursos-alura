bfa005cc88cfcb5ddb80c541d3a389e8ad71a259
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-20 14:07:51 -0300

    feat(React-Abstraindo-CSS-StyledComponents): class 01/06 - Styled Components

--- React-Abstraindo-CSS-StyledComponents/smart-bank/src/Components/Titulo/index.jsx
+++ React-Abstraindo-CSS-StyledComponents/smart-bank/src/Components/Titulo/index.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import styled from 'styled-components';
+
+const Titulo = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`;
 
 const Titulo = ({ children }) => {
   return <h1 className="titulo">{children}</h1>;
