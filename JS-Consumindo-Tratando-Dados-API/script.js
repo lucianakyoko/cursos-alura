758f37acfd1bbf1fb4bc24a2c9448a4040e16f4f
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-21 19:44:26 -0300

    feat(JS-Consumindo-Tratano-Dados-API): class 01 - Entendendo o JavaScript

---
+++ JS-Consumindo-Tratando-Dados-API/script.js
@@ -0,0 +1,10 @@
+console.log('mandando ou pro amigo')
+
+function mandaMensagem() {
+  console.log('Tudo bem')
+  console.log('vou te mandar uma solicitação')
+  console.log('solicitação recebida!')
+}
+
+mandaMensagem()
+console.log('até mais')
