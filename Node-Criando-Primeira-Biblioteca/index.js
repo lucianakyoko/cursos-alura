7a8d5bfc19c702c2118554e2e733925dee9684f6
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-21 16:41:19 -0300

    feat(Node-Criando-Primeira-Biblioteca) - class 02 - Carregamento de arquivos

--- Node-Criando-Primeira-Biblioteca/index.js
+++ Node-Criando-Primeira-Biblioteca/index.js
@@ -1,5 +1,40 @@
+import fs from 'fs';
 import chalk from 'chalk';
-console.log(chalk.blue('hello world!'))
-console.log('olá mundo');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API');
+
+function trataErro(erro) {
+  console.log(erro)
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'))
+}
+
+//async await
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.blue(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+//Promises com them()
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then(texto => console.log(chalk.green(texto)))
+//     .catch(trataErro)
+// }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if(erro) {
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto))
+//   });
+// }
+
+pegaArquivo('./arquivos/texto.md');
+// pegaArquivo('./arquivos/');
