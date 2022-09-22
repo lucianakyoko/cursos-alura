b28f7a39d9bfbd099937ba5bf5739534b57b497e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-22 15:51:45 -0300

    feat(Nextjs-Explorando-Framework): class 03 - Estilizando o projeto

--- Nextjs-Explorando-Framework/pages/index.js
+++ Nextjs-Explorando-Framework/pages/index.js
@@ -1,10 +1,30 @@
-import Link from '../src/components/Link';
+import HomeScreen from "../src/screens/HomeScreen";
+export default HomeScreen;
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura Cases - Home Page</h1>
-      <Link href='/faq'>Ir para FAQ</Link>
-    </div>
-  )
-}
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({children, as}) {
+//   const Tag = as;
+//   return(
+//     <>
+//       <Tag>{children}</Tag>
+      
+//       <style jsx>{`
+//         ${Tag} {
+//           color: red;
+//         }
+//       `}</style>
+//     </>
+//   )
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h2">Alura Cases - Home Page</Title>
+//       <Link href='/faq'>Ir para FAQ</Link>
+//       <Footer />
+//     </div>
+//   )
+// }
