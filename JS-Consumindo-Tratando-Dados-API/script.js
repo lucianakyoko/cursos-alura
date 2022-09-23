6db2b144dcf3e43896fa788aa5d0cf0bef22e308
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-23 16:01:58 -0300

    feat(JS-Consumindo-Tratando-Dados-API): class 03 - Aprofundando em promises

--- JS-Consumindo-Tratando-Dados-API/script.js
+++ JS-Consumindo-Tratando-Dados-API/script.js
@@ -1,2 +1,13 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+  .then(resposta => resposta.json())
+  .then(res => {
+    if(res.erro) {
+      throw Error('Esse CEP não existe');
+    } else {
+    console.log(res)
+    }
+  })
+  .catch(erro => console.log(erro))
+  .finally(mensagem = console.log('processamento concluido'));
+
 console.log(consultaCEP)
