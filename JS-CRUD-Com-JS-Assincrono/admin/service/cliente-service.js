70edbff734e07f111013890f47fa9279cb33f979
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-04 14:30:33 -0300

    feat(JS-CRUD-Com-JS-Assincrono): class 02 - Fetch API

--- JS-CRUD-Com-JS-Assincrono/admin/service/cliente-service.js
+++ JS-CRUD-Com-JS-Assincrono/admin/service/cliente-service.js
@@ -1,45 +1,8 @@
-const criaNovaLinha = (nome, email) => {
-  const linhaNovoCliente = document.createElement('tr');
-
-  const conteudo = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-      <ul class="tabela__botoes-controle">
-        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-      </ul>
-    </td> 
-  `
-
-  linhaNovoCliente.innerHTML = conteudo;
-  return linhaNovoCliente;
-};
-
-const tabela = document.querySelector('[data-tabela]');
-
 const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    
-    http.open('GET', 'http://localhost:3000/profile');
-
-    http.onload = () => {
-      if(http.status >= 400) {
-        reject(JSON.parse(http.response))
-      } else {
-        resolve(JSON.parse(http.response))
-      }
-    };
-
-    http.send();
-  })
-  return promise;
+  return fetch(`http://localhost:3000/profile`)
+    .then(resposta => resposta.json());
 };
 
-listaClientes()
-  .then(data => {
-    data.forEach(element => {
-      tabela.appendChild(criaNovaLinha(element.nome, element.email));
-    });
-  });
+export const clienteService = {
+  listaClientes,
+}
