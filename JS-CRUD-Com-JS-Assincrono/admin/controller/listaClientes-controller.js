792c9441b19296692d18c76d9634f278a93fe501
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-05 11:04:10 -0300

    feat(JS-CRUD-Com-JS): class 03 - criar e remover clientes

--- JS-CRUD-Com-JS-Assincrono/admin/controller/listaClientes-controller.js
+++ JS-CRUD-Com-JS-Assincrono/admin/controller/listaClientes-controller.js
@@ -1,6 +1,6 @@
 import { clienteService } from '../service/cliente-service.js';
 
-const criaNovaLinha = (nome, email) => {
+const criaNovaLinha = (nome, email, id) => {
   const linhaNovoCliente = document.createElement('tr');
 
   const conteudo = `
@@ -15,14 +15,27 @@ const criaNovaLinha = (nome, email) => {
   `
 
   linhaNovoCliente.innerHTML = conteudo;
+  linhaNovoCliente.dataset.id = id;
   return linhaNovoCliente;
 };
 
 const tabela = document.querySelector('[data-tabela]');
 
+tabela.addEventListener('click', (evento) => {
+  let ehBotaoDeletar = evento.target.className === 'botao-simples botao-simples--excluir';
+
+  if(ehBotaoDeletar) {
+    const linhaCliente = evento.target.closest('[data-id]');
+    let id = linhaCliente.dataset.id;
+    clienteService.removeCliente(id).then(() => {
+      linhaCliente.removeCliente()
+    });
+  }
+});
+
 clienteService.listaClientes()
   .then(data => {
     data.forEach(element => {
-      tabela.appendChild(criaNovaLinha(element.nome, element.email));
+      tabela.appendChild(criaNovaLinha(element.nome, element.email, element.id));
     });
   });
